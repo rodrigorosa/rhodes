f83bc6c436ac191a5e04542819b59d14b2b257b8
Leonardo Falk <leonardofalk@users.noreply.github.com>
2017-07-25 16:01:23 -0300

    Removida regra de validação de inscrição estadual para o estado de Alagoas

--- lib/framework/br_inscricao_estadual/al.rb
+++ lib/framework/br_inscricao_estadual/al.rb
@@ -1,15 +1,20 @@
 module BrInscricaoEstadual
-	class AL < Common
+  class AL < Common
     def valid?
-     super && type
+      super && type
     end
 
     protected
-      STATE_CODE = "24"
-      DIGITS_ZERO = [10]
 
-      def type  
-        %w(0 3 5 7 8).include?(self.insc_est[2])
-      end
-	end
+    STATE_CODE = "24"
+    DIGITS_ZERO = [10]
+
+    def type  
+      # %w(0 3 5 7 8).include?(self.insc_est[2])
+      # validação removida pois existem casos de inscricoes validas
+      # com digitos 6 ou 4, porem o site do estado de alagoas nao
+      # disponibiliza informacao atualizada sobre a validacao
+      true
+    end
+  end
 end
