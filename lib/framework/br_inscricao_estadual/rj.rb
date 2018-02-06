368606cd2894ddecdc0b344d1fe935beb6d5261d
Leonardo Falk <leonardofalk@users.noreply.github.com>
2018-02-06 10:57:27 -0200

    Correção digito verificador IE-RJ

--- lib/framework/br_inscricao_estadual/rj.rb
+++ lib/framework/br_inscricao_estadual/rj.rb
@@ -2,7 +2,8 @@ module BrInscricaoEstadual
   class RJ < Common
     protected
 
-      PESO = [2, 7, 6, 5, 4, 3, 2]
-      STATE_INSC_SIZE = 8
+    DIGITS_ZERO = [0, 1]
+    PESO = [2, 7, 6, 5, 4, 3, 2]
+    STATE_INSC_SIZE = 8
   end
 end
